c7290ddcb42e7c2d5e1a60ec4c44e165f5d94ad3
ekstensi2018 <49778183+ekstensi2018@users.noreply.github.com>
2019-04-19 14:20:36 +0700

    Ini adalah latihan 1
    Berisi tentang kenaikan gaji dalam java

---
+++ new_salary.java
@@ -0,0 +1,18 @@
+/**
+ * Latihan 1
+ *
+ * 
+ */
+public class new_salary {
+ public static void main(String[] args) {
+  // deklarasi variabel
+  double salary = 100;
+  double newSalary = 10;
+
+  final double RAISE_RATE = 1.10;
+  newSalary = salary * RAISE_RATE;
+  System.out.println("Current salary: " + salary + " After raise: " +
+   newSalary);
+ }
+
+}
